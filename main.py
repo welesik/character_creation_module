712730d022b6c6b3ca8feadbbab20a3da621633f
Илья Киселёв <iwelesik@mail.ru>
2023-03-07 21:53:07 +1000

    From works

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from random import randint
 
+from graphic_arts.start_game_banner import run_screensaver
+
 
 def attack(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
@@ -82,7 +84,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -91,7 +94,7 @@ def main():
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
-    print(start_training(char_name, char_class))
+    print(start_training(char_name, char_class)) 
 
 
 main()
